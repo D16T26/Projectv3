e92bb28563f0ee488edb398b17de66ed5e7e2960
Sondre Grøneng <songro93@gmail.com>
2016-10-21 23:16:05 +0200

    Got Navbutton mostly working. inverting colors, got- and lostfocus events, etc.

--- Game_Project/Custom_Controls/NavButton.vb
+++ Game_Project/Custom_Controls/NavButton.vb
@@ -1,9 +1,41 @@
 ﻿Public Class NavButton
     Inherits System.Windows.Forms.Button
+    Private Property Selected As Boolean = False
 
     Public Sub New()
         Me.BackColor = Color.Black
         Me.ForeColor = Color.White
+        Me.Font = New Font(FontFamily.GenericMonospace, 10)
+        Me.Size = New Size(140, 30)
+        Me.FlatStyle = FlatStyle.Flat
+        Me.FlatAppearance.BorderSize = 0
+    End Sub
 
+    Private Sub AtGotFocus(sender As Game_Project.NavButton, e As EventArgs) Handles Me.GotFocus
+        Me.Selected = True
+        Me.Selecting()
+    End Sub
+
+    Private Sub AtLostFocus(sender As Game_Project.NavButton, e As EventArgs) Handles Me.LostFocus
+        Me.Selected = False
+        Me.InvertColors()
+    End Sub
+
+    Private Sub AtMouseEnter(sender As Game_Project.NavButton, e As EventArgs) Handles Me.MouseEnter
+        Me.Focus()
+    End Sub
+
+    Private Sub Selecting()
+        InvertColors()
+    End Sub
+
+    Private Sub InvertColors()
+        If Me.Selected Then
+            Me.BackColor = Color.White
+            Me.ForeColor = Color.Black
+        Else
+            Me.BackColor = Color.Black
+            Me.ForeColor = Color.White
+        End If
     End Sub
 End Class
--- Game_Project/Form1.Designer.vb
+++ Game_Project/Form1.Designer.vb
@@ -23,14 +23,47 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.NavButton1 = New Game_Project.NavButton()
+        Me.NavButton2 = New Game_Project.NavButton()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
+        Me.Panel1.Controls.Add(Me.NavButton2)
+        Me.Panel1.Controls.Add(Me.NavButton1)
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
         Me.Panel1.Size = New System.Drawing.Size(640, 480)
         Me.Panel1.TabIndex = 0
+        '
+        'NavButton1
+        '
+        Me.NavButton1.BackColor = System.Drawing.Color.Black
+        Me.NavButton1.FlatAppearance.BorderSize = 0
+        Me.NavButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.NavButton1.Font = New System.Drawing.Font("Courier New", 10.0!)
+        Me.NavButton1.ForeColor = System.Drawing.Color.White
+        Me.NavButton1.Location = New System.Drawing.Point(226, 123)
+        Me.NavButton1.Name = "NavButton1"
+        Me.NavButton1.Size = New System.Drawing.Size(140, 30)
+        Me.NavButton1.TabIndex = 0
+        Me.NavButton1.Text = "NavButton1"
+        Me.NavButton1.UseVisualStyleBackColor = False
+        '
+        'NavButton2
+        '
+        Me.NavButton2.BackColor = System.Drawing.Color.Black
+        Me.NavButton2.FlatAppearance.BorderSize = 0
+        Me.NavButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.NavButton2.Font = New System.Drawing.Font("Courier New", 10.0!)
+        Me.NavButton2.ForeColor = System.Drawing.Color.White
+        Me.NavButton2.Location = New System.Drawing.Point(226, 185)
+        Me.NavButton2.Name = "NavButton2"
+        Me.NavButton2.Size = New System.Drawing.Size(140, 30)
+        Me.NavButton2.TabIndex = 1
+        Me.NavButton2.Text = "NavButton2"
+        Me.NavButton2.UseVisualStyleBackColor = False
         '
         'Form1
         '
@@ -40,9 +73,12 @@ Partial Class Form1
         Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents Panel1 As Panel
+    Friend WithEvents NavButton2 As NavButton
+    Friend WithEvents NavButton1 As NavButton
 End Class
