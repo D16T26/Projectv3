09713220a4db98ab40c2810a72b953621e0efa48
Sondre Grøneng <songro93@gmail.com>
2016-10-27 20:52:35 +0200

    Added mainmenu and introframe and their respective actions to projectconstants.

--- Game_Project/ProjectConstants.vb
+++ Game_Project/ProjectConstants.vb
@@ -4,6 +4,8 @@
     Public Const ContentWidth As Integer = 640
     Public Const ContentHeight As Integer = 480
 
+    Public ReadOnly Property mainMenu As MainMenu
+    Public ReadOnly Property introFrame As IntroFrame
     Public ReadOnly Property Floor1RoomA As Room
     Public ReadOnly Property Floor2RoomA As Room
     Public ReadOnly Property Floor3RoomA As Room
@@ -13,15 +15,19 @@
 
     Public Sub New(Owner As Form1)
         Me.Owner = Owner
-        Me.Floor1RoomA = New F1RA(Owner)
-        Me.Floor2RoomA = New F2RA(Owner)
-        Me.Floor3RoomA = New F3RA(Owner)
-        Me.Floor4RoomA = New F4RA(Owner)
-        Me.Floor5RoomA = New F5RA(Owner)
-        Me.Floor6RoomA = New F6RA(Owner)
+        Me.mainMenu = New MainMenu(Me.Owner)
+        Me.introFrame = New IntroFrame(Me.Owner)
+        Me.Floor1RoomA = New F1RA(Me.Owner)
+        Me.Floor2RoomA = New F2RA(Me.Owner)
+        Me.Floor3RoomA = New F3RA(Me.Owner)
+        Me.Floor4RoomA = New F4RA(Me.Owner)
+        Me.Floor5RoomA = New F5RA(Me.Owner)
+        Me.Floor6RoomA = New F6RA(Me.Owner)
     End Sub
 
     Friend Sub Dispose()
+        Me.mainMenu.Dispose()
+        Me.introFrame.Dispose()
         Me.Floor1RoomA.Dispose()
         Me.Floor2RoomA.Dispose()
         Me.Floor3RoomA.Dispose()
