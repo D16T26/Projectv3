cc5c98639bccf31224e2da9d898d21863cd27317
Sondre Grøneng <songro93@gmail.com>
2016-10-27 20:34:14 +0200

    Initialised all main rooms.

---
+++ Game_Project/Frames/Rooms/F3RA.vb
@@ -0,0 +1,10 @@
+﻿Public Class F3RA
+    Public Sub New(owner As Form1)
+        MyBase.New(owner)
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+End Class
