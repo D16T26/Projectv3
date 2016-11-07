04d946fd1c0c5bf02660f54b4ae4b836ce038761
Sondre Grøneng <songro93@gmail.com>
2016-11-07 19:11:45 +0100

    Finalized Floor 4

--- Game_Project/Frames/Rooms/F4RA.vb
+++ Game_Project/Frames/Rooms/F4RA.vb
@@ -1,47 +1,52 @@
 ﻿Public Class F4RA
     Public ReadOnly Property author As String = "Audun Flage"
+
     Public Sub New(owner As Form1)
         MyBase.New(owner)
         InitializeComponent()
         Me.Initialize()
     End Sub
 
+    ''' <summary>
+    ''' Initialises local data for the room. Dialogue text and setting
+    ''' the answers to the buttons
+    ''' </summary>
     Private Sub Initialize()
         Me.Label6.Text = "Du er nå i fjerde etasje men her møter du også på en " & Environment.NewLine &
                          "dørvakt i veien, han krever du svarer på et spørsmål " & Environment.NewLine &
                          "å komme forbi."
+
+        Me.NavButton1.Text = "Foran deg"
+        Me.NavButton2.Text = "Til høyre"
+        Me.NavButton3.Text = "Bak deg"
+        Me.NavButton4.Text = "Til venstre"
     End Sub
 
-
+    ''' <summary>
+    ''' This handles dialogue procession. In this particular room there is no extra
+    ''' dialogue before the riddle so it simply initialises the riddle.
+    ''' it used to check for whether or not the riddle had been completed.
+    ''' the check was removed due to the fact that the button is hidden completely
+    ''' once the riddle starts and never shows up again.
+    ''' </summary>
     Private Sub Button1_Click1(sender As Button, e As EventArgs) Handles DialogueButton.Click
-        If Not riddleWon Then
-            Me.StartRiddle()
-        End If
+        Me.StartRiddle()
     End Sub
 
+    ''' <summary>
+    ''' This procedure initialises the riddle itself by setting the display to the
+    ''' the riddle and showing the navbuttons. navbutton1 is also set as focus so
+    ''' the player do not have to use the mouse. the mouse is evil.
+    ''' </summary>
     Private Sub StartRiddle()
-        With Me
-            .DialogueButton.Hide()
-            .Label6.Text = """Hvis du ser rett på en regnbue, hvor er sola da?" & Environment.NewLine
-            .DialogueButton.Hide()
-            With .NavButton1
-                .Show()
-                .Text = "Foran deg"
-                .Focus()
-            End With
-            With .NavButton2
-                .Show()
-                .Text = "Til høyre"
-            End With
-            With .NavButton3
-                .Show()
-                .Text = "Bak deg"
-            End With
-            With .NavButton4
-                .Show()
-                .Text = "Til venstre"
-            End With
-        End With
+        Me.DialogueButton.Hide()
+        Me.Label6.Text = """Hvis du ser rett på en regnbue, hvor er sola da?" & Environment.NewLine
+
+        Me.NavButton1.Show()
+        Me.NavButton2.Show()
+        Me.NavButton3.Show()
+        Me.NavButton4.Show()
+        Me.NavButton1.Focus()
     End Sub
 
     ''' <summary>
@@ -57,7 +62,6 @@
             "being to suffer the existence pop-up messages!!"
         Dim title As String = "L2Math mate"
         Dim style As MsgBoxStyle = MsgBoxStyle.Critical
-
         MsgBox(text, style, title)
     End Sub
 
@@ -67,13 +71,11 @@
     ''' and changes the display text to a message indicating that the player can procede.
     ''' </summary>
     Private Sub CorrectAnswer_Click(sender As NavButton, e As EventArgs) Handles NavButton3.Click
-        With Me
-            .riddleWon = True
-            .NavButton1.Hide()
-            .NavButton2.Hide()
-            .NavButton3.Hide()
-            .NavButton4.Hide()
-            .Label6.Text = "Er vel bare å fortsette videre..."
-        End With
+        Me.riddleWon = True
+        Me.NavButton1.Hide()
+        Me.NavButton2.Hide()
+        Me.NavButton3.Hide()
+        Me.NavButton4.Hide()
+        Me.Label6.Text = "Er vel bare å fortsette videre..."
     End Sub
 End Class
